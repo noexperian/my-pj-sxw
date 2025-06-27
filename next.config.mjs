cc453515e02cd29886ffeb8df68fb7d18e2c22d9
noexperian <154860173+noexperian@users.noreply.github.com>
2025-06-27 08:44:44 +0700

    baba

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,10 @@
+
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  output: 'export',
+  images: {
+    unoptimized: true, // ถ้าใช้ <Image /> ของ Next.js ต้องใส่
+  },
+};
 
 export default nextConfig;
